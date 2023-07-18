b07f28908b7f6b72521b4a1d88c7b8596c11f894
jgrove90 <jgrove90@gmail.com>
2023-07-18 16:42:40 -0600

    added location and episode transformations

--- src/app_utils.py
+++ src/app_utils.py
@@ -16,8 +16,19 @@ def getID(data: dict) -> int:
     else:
         return None
 
+def getIDList(data: List) -> List:
+    ids = []
+    for url in data:
+        parts = url.split("/")
+        if parts[-1].isdigit():
+            url = int(parts[-1])
 
 def dateTimeParse(date: str):
     datetime_format = "%Y-%m-%dT%H:%M:%S.%fZ"
     datetime_obj = datetime.strptime(date, datetime_format)
     return datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
+
+def dateParse(date: str):
+    datetime_format = "%B %d, %Y"
+    datetime_obj = datetime.strptime(date, datetime_format)
+    return datetime_obj.strftime("%Y-%m-%d")
--- src/transformations.py
+++ src/transformations.py
@@ -14,26 +14,38 @@ class Transformations:
         df = createDataFrame(self.data)
         return df
 
+    def bronzeCharacter(self):
+        df = self.createDataFrame().drop(columns=["type", "url"])
+        return df
+
     def silverCharacter(self):
-        
         df = (
-            self.data[
-                [
-                    "id",
-                    "name",
-                    "status",
-                    "species",
-                    "gender",
-                    "origin",
-                    "location",
-                    "image",
-                    "created",
-                ]
-            ]
+            self.data.drop(columns=["episode"])
             .assign(origin=self.data.origin.apply(lambda _df: getID(_df)))
             .assign(location=self.data.location.apply(lambda _df: getID(_df)))
             .assign(created=self.data.created.apply(lambda _df: dateTimeParse(_df)))
             .astype({"origin": pd.Int64Dtype(), "location": pd.Int64Dtype()})
             .rename(columns={"origin": "origin_id", "location": "location_id"})
+        )
+        return df
+
+    def bronzeLocation(self):
+        df = self.createDataFrame().drop(columns=["residents", "url"])
+        return df
+
+    def silverLocation(self):
+        df = self.data.assign(
+            created=self.data.created.apply(lambda _df: dateTimeParse(_df))
+        )
+        return df
+
+    def bronzeEpisode(self):
+        df = self.createDataFrame().drop(columns="url")
+        return df
+
+    def silverEpisode(self):
+        df = self.data.assign(
+            air_date=self.data.air_date.apply(lambda _df: dateParse(_df)),
+            created=self.data.created.apply(lambda _df: dateTimeParse(_df))
         )
         return df
