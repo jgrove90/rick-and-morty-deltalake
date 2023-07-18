944bab39acf3683b0aa04f73ea3d5013ea3071ba
jgrove90 <jgrove90@gmail.com>
2023-07-17 18:10:39 -0600

    added delta table paths

--- src/table_manager.py
+++ src/table_manager.py
@@ -5,6 +5,12 @@ from pyarrow import Schema
 BRONZE_CHARACTERS_TABLE = "./deltalake/rick_and_morty/bronze/characters"
 SILVER_CHARACTERS_TABLE = "./deltalake/rick_and_morty/silver/characters"
 
+BRONZE_LOCATION_TABLE = "./deltalake/rick_and_morty/bronze/location"
+SILVER_LOCATION_TABLE = "./deltalake/rick_and_morty/silver/location"
+
+BRONZE_EPISODES_TABLE = "./deltalake/rick_and_morty/bronze/episodes"
+SILVER_EPISODES_TABLE = "./deltalake/rick_and_morty/silver/episodes"
+
 
 class DeltaTableManager:
     def __init__(self, dataframe):
