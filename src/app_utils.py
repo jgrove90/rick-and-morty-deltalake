b9a3dcc3756e035bea9ea460636a3a051f2c4745
jgrove90 <jgrove90@gmail.com>
2023-07-21 15:28:04 -0600

    added support for np.ndarrays under getID()

--- src/app_utils.py
+++ src/app_utils.py
@@ -1,18 +1,33 @@
 import re
 from typing import List
+import pandas as pd
 from pandas import DataFrame, to_datetime
 from datetime import datetime
-from api_requests import *
+from extract import *
+import numpy as np
+
+REGEX_URL_ID = r"\d+$"
 
 
 def createDataFrame(data: List[dict]) -> DataFrame:
     return DataFrame(data)
 
 
-def getID(data: dict) -> int:
-    url = data.get("url")
-    if url:
-        return re.search(r"\d+$", url).group()
+##TODO FIX
+def getID(data: dict | np.ndarray) -> int:
+    if isinstance(data, dict):
+        url = data.get("url")
+        if url:
+            return int(re.search(REGEX_URL_ID, data).group())
+        else:
+            return None
+    elif isinstance(data, np.ndarray):
+        char_id = []
+        data = data.tolist()
+        for url in data:
+            char = re.search(REGEX_URL_ID, url).group()
+            char_id.append(int(char))
+        return char_id
     else:
         return None
 
@@ -27,3 +42,4 @@ def dateFormat(date: str):
     datetime_format = "%B %d, %Y"
     datetime_obj = datetime.strptime(date, datetime_format)
     return datetime_obj.strftime("%Y-%m-%d")
+
